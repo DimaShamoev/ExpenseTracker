04038d2e399a6a0f5d1a8c999e7bcd38f2a95087
Dima Shamoev <dimashamoev5@gmail.com>
2025-02-25 11:39:13 +0400

    create transaction UI page and little bit functionality on client side

---
+++ client/src/components/TransactionForm.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa'
+import { Form, useLoaderData } from 'react-router-dom'
+import { IResponseTransactionLoader } from '../Types/types'
+import CategoryModal from './modals/CategoryModal'
+
+const TransactionForm: React.FunctionComponent = () => {
+    const { categories } = useLoaderData() as IResponseTransactionLoader
+    const [visibleModal, setVisibleModal] = React.useState<boolean>(false)
+
+    return (
+        <div className='rounded-md bg-slate-800 p-4'>
+            <Form
+                className='grid gap-4'
+                method='post'
+                action='/transactions'
+            >
+
+                <label className='grid' htmlFor="title">
+                    <span>Title</span>
+                    <input 
+                        className='input'
+                        type="text"
+                        placeholder='Enter Title'
+                        name='title'
+                        required
+                    />
+                </label>
+                <label className='grid' htmlFor="amount">
+                    <span>Amount</span>
+                    <input 
+                        className='input'
+                        type="number"
+                        placeholder='Enter Amount'
+                        name='amount'
+                        required
+                    />
+                </label>
+
+                {categories.length ? (
+                    <label htmlFor="category" className='grid'>
+                        <span>Category</span>
+                        <select className='input' name="category" required>
+                            {categories.map((category, index) => (
+                                <option key={index} value={category.id}>{category.title}</option>
+                            ))}
+                        </select>
+                    </label>
+                ) : (
+                    <h1 className='mt-1 text-red-600'>To Continue Create Category</h1>
+                )}
+                
+
+                {/* Add Category */}
+                <button
+                    onClick={() => setVisibleModal(true)} 
+                    className="cursor-pointer mt-2 max-w-fit flex items-center gap-2 text-white/50 hover:text-white"
+                >
+                    <FaPlus />
+                    <span>Manage Categories</span>
+                </button>
+
+                {/* Radio Buttons */}
+                <div className="flex gap-4 items-center">
+                    <label className='flex cursor-pointer items-center gap-2' htmlFor="type">
+                        <input
+                            type="radio" 
+                            name='type'
+                            value={'income'}
+                            className='form-radio text-blue-600'
+                        />
+                        <span>Income</span>
+                    </label>
+                    <label className='flex cursor-pointer items-center gap-2' htmlFor="type">
+                        <input
+                            type="radio" 
+                            name='type'
+                            value={'expense'}
+                            className='form-radio text-blue-600'
+                        />
+                        <span>expense</span>
+                    </label>
+                </div>
+
+                {/* Submit btn */}
+                <div
+                >
+                    <button
+                        type="submit" // Add this line to make it a submit button
+                        className="btn btn-green flex text-center items-center justify-center cursor-pointer"
+                    >
+                        Submit
+                    </button>
+                </div>
+                
+            </Form>
+
+            {visibleModal && (
+                <CategoryModal type='post' setVisibleModal={setVisibleModal} />
+            )}
+
+        </div>
+    )
+}
+
+export default TransactionForm
--- client/src/pages/Transactions.tsx
+++ client/src/pages/Transactions.tsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import TransactionForm from '../components/TransactionForm'
+import { instance } from '../api/axios.api'
+import { ICategory } from '../Types/types'
+import { toast } from 'react-toastify'
+import TransactionTable from '../components/TransactionTable'
+
+export const transactionAction = async ({request}: any) => {
+    switch(request.method) {
+        case 'POST': {
+            const formData = await request.formData()
+            const newTransaction = {
+                title: formData.get('title'),
+                amount: +formData.get('amount'),
+                category: formData.get('category'),
+                type: formData.get('type')
+            }
+
+            await instance.post('/transactions', newTransaction);
+            toast.success("Transaction Added")
+            return null
+        }
+        case "DELETE": {
+            
+        }
+    }
+}
+
+export const transactionLoader = async () => {
+    const categories = await instance.get<ICategory[]>('/categories')
+
+    const data = {
+        categories: categories.data,
+    }
+
+    return data
+}
 
 const Transactions: React.FunctionComponent = () => {
+
     return (
-        <div>Transactions</div>
+        <>
+            <div className="grid grid-cols-3 gap-4 mt-4 items-start">
+                {/* Add Transaction Form */}
+                <div className="grid col-span-2">
+                    <TransactionForm />
+                </div>
+
+                {/* Statistic Block */}
+                <div className="rounded-md bg-slate-800 p-3">
+                    <div className="grid grid-cols-2 gap-3">
+                        <div>
+                            <p className='uppercase text-md font-bold text-center'>
+                                Total Income
+                            </p>
+                            <p className="bg-green-600 p-1 rounded-sm text-center mt-2">
+                                1000$
+                            </p>
+                        </div>
+                        <div>
+                            <p className='uppercase text-md font-bold text-center'>
+                                Total Expense
+                            </p>
+                            <p className="bg-red-500 p-1 rounded-sm text-center mt-2">
+                                1000$
+                            </p>
+                        </div>
+                    </div>
+
+                    <>
+                        Chart
+                    </>
+                </div>
+            </div>
+            
+            {/* Result Table */}
+            <h1 className='mt-5'>Table</h1>
+            <TransactionTable />
+
+        </>
     )
 }
 
